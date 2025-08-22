47f9c1cc5e3c8301ac55010753585e22614c2545
Shravan kumar <shravankumar3849@gmail.com>
2025-08-22 22:04:52 +0530

    fixed dark mode issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-  darkMode: 'class', // follow system preference
+  darkMode: "media", // now it follows system dark/light
   content: [
-  "./src/**/*.{js,jsx,ts,tsx}",
-  "./src/pages/**/*.{js,jsx,ts,tsx}", // optional redundancy
-  "./src/pages/**/*.{js,jsx,ts,tsx}", // optional redundancy
-  "./src/pages/DashbordPages/*.{js,jsx,ts,tsx}", // optional redundancy
-  "./src/pages/userpages/*.{js,jsx,ts,tsx}", // optional redundancy
-  "./public/index.html",
-],
-
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
