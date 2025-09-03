505b7bc49fc51d5ce1bdef22b91f3416541ca0e7
Shravan kumar <shravankumar3849@gmail.com>
2025-09-03 19:49:50 +0530

    adminDashboard api change

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -6,5 +6,10 @@ const api = axios.create({
   baseURL: `${BACKEND_URL}/api`,
   headers: { "Content-Type": "application/json" },
 });
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 export default api;
