548ac2961abbc3da373e39404ec7fa7f58c5f261
Shravan kumar <shravankumar3849@gmail.com>
2025-09-08 21:05:28 +0530

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,9 +17,20 @@ const HandleClickLogo = ()=>{
 const hideProfileIcon = location.pathname === "/login"
 
   return(
-    <header className="h-16 flex justify-between items-center bg-indigo-600 dark:bg-gray-800 px-6 shadow">
-        <h1 className="text-xl sm:text-2xl font-bold text-white"><span className="cursor-pointer" onClick={HandleClickLogo}>📱 </span  > <span>Universal Combo</span></h1>
-           {!hideProfileIcon && <ProfileIcon></ProfileIcon>}
-    </header>
+   <header className="h-16 flex justify-between items-center bg-indigo-600 dark:bg-gray-800 px-6 shadow">
+  {/* Logo + Title */}
+  <div className="flex items-center gap-2 cursor-pointer" onClick={HandleClickLogo}>
+    <img
+      src="/UniversalCombo.jpg"
+      alt="Universal Combo Logo"
+      className="h-10 w-10 rounded-full"
+    />
+    <h1 className="text-xl sm:text-2xl font-bold text-white">Universal Combo</h1>
+  </div>
+
+  {/* Profile Icon */}
+  {!hideProfileIcon && <ProfileIcon />}
+</header>
+
   )
 }
